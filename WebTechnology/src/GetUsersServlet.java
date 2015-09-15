4163f4ba045c6054fff00b598d151d57593f1cc2
Viradj <viradj_13@hotmail.com>
2015-09-15 13:04:39 +0200

    finished SearchRoomServlet and GetUsersServlet

--- WebTechnology/src/GetUsersServlet.java
+++ WebTechnology/src/GetUsersServlet.java
@@ -2,9 +2,12 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -33,7 +36,7 @@ public class GetUsersServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if(!AuthHelper.isVerhuurderIngelogd(request, response))
+		if(!AuthHelper.isAdminIngelogd(request, response))
 		{
 			return;
 		}
@@ -42,18 +45,45 @@ public class GetUsersServlet extends HttpServlet {
 		
 		PrintWriter writer = response.getWriter();
 		
-		writer.append("<p>Aantal keren bezocht: </p>"); //+ iets met cookies
+		int visitCount = 0;
+		String lastVisit = "";
+		
+		Cookie[] cookies = request.getCookies();
+		
+		for(Cookie c : cookies){
+			if(c.getName().equals("visitCount")){
+				visitCount = Integer.parseInt(c.getValue());
+			}
+			else if(c.getName().equals("lastVisit")){
+				lastVisit = c.getValue();
+			}
+		}
+		
+		writer.append("<p>Aantal keren bezocht: " + visitCount + "</p>");
+		writer.append("<p>Laatste keer bezocht: " + lastVisit + "</p><br />");
 		writer.append("<table>"
 				+ "<tr><th>Gebruikers</th></tr>"
 				); 
 		for(User user : kamerVerhuur.getUsers()){
 			writer.append(String.format(""
 					+ "<tr>"
-					+ "<td>%d</td>"
+					+ "<td>%s</td>"
 					+ "</tr>",
 					user.getUsername()));
 		}
 		writer.append("</table>");
+		
+		visitCount++;
+		
+		Cookie newVisitCookie = new Cookie("visitCount", "" + visitCount);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+		Cookie lastVisitCookie = new Cookie("lastVisit", sdf.format(new Date()));
+		
+		newVisitCookie.setMaxAge(84600);
+		lastVisitCookie.setMaxAge(84600);
+		
+		response.addCookie(newVisitCookie);
+		response.addCookie(lastVisitCookie);
 	}
 
 	/**
--- WebTechnology/src/KamerVerhuurServletContextListener.java
+++ WebTechnology/src/KamerVerhuurServletContextListener.java
@@ -3,7 +3,10 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 import model.Admin;
+import model.Huurder;
+import model.Kamer;
 import model.KamerVerhuur;
+import model.Verhuurder;
 
 @WebListener
 public class KamerVerhuurServletContextListener implements ServletContextListener {
@@ -13,7 +16,25 @@ public class KamerVerhuurServletContextListener implements ServletContextListene
     public void contextInitialized(ServletContextEvent arg0)  { 
     	KamerVerhuur kamerVerhuur = new KamerVerhuur();
     	Admin admin = new Admin("Admin","test123");
+    	Verhuurder verhuurder = new Verhuurder("Verhuurder", "test123");
+    	Huurder huurder = new Huurder("Huurder", "test123");
     	kamerVerhuur.addUser(admin);
+    	kamerVerhuur.addUser(verhuurder);
+    	kamerVerhuur.addUser(huurder);
+    	
+    	Kamer kamer1 = new Kamer(1, 20, 500.0, "Enschede", 5);
+    	Kamer kamer2 = new Kamer(2, 20, 500.0, "Enschede", 3);
+    	Kamer kamer3 = new Kamer(3, 20, 700.0, "Enschede", 5);
+    	Kamer kamer4 = new Kamer(4, 20, 500.0, "Hengelo", 5);
+    	Kamer kamer5 = new Kamer(5, 12, 500.0, "Enschede", 5);
+    	
+    	kamerVerhuur.addKamer(kamer1);
+    	kamerVerhuur.addKamer(kamer2);
+    	kamerVerhuur.addKamer(kamer3);
+    	kamerVerhuur.addKamer(kamer4);
+    	kamerVerhuur.addKamer(kamer5);
+    	
+    	
     	arg0.getServletContext().setAttribute("KamerVerhuur",kamerVerhuur );
     	    	
     }
